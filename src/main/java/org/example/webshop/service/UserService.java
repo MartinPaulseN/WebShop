a4bd83630a36ff1233338203b579f068d393e452
ITHS <martinpaulsen617@gmail.com>
2025-05-13 18:57:53 +0200

    Skapat filen UserService för mappen service

---
+++ src/main/java/org/example/webshop/service/UserService.java
@@ -0,0 +1,28 @@
+package org.example.webshop.service;
+
+
+import org.example.webshop.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void register(UserDto dto) {
+        User user = new User();
+        user.setUsername(dto.getUsername());
+        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
+}

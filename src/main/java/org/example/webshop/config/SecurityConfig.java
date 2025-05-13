aeb0120df1fad38ccf91f513cf47bc21332009eb
ITHS <martinpaulsen617@gmail.com>
2025-05-13 20:23:03 +0200

    Skapat filen SecurityConfig i paketet config

---
+++ src/main/java/org/example/webshop/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.example.webshop.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/register", "/login", "/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.loginPage("/login").permitAll())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .headers(headers -> headers.frameOptions().sameOrigin());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

aee1561665f41182684dffc130a7c1ecbca6a723
ITHS <martinpaulsen617@gmail.com>
2025-05-13 18:49:22 +0200

    Skapat interfacet UserRepository för mappen Repository

---
+++ src/main/java/org/example/webshop/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package org.example.webshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}

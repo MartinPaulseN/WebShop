5cbd1e2175134bc3aab6b95f28580a2ca3a85fa4
ITHS <martinpaulsen617@gmail.com>
2025-05-13 19:05:52 +0200

    Skapat filen LoginController för mappen controller

---
+++ src/main/java/org/example/webshop/controller/LoginController.java
@@ -0,0 +1,32 @@
+package org.example.webshop.controller;
+
+
+import org.example.webshop.model.UserDto;
+import org.example.webshop.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("userDto", new UserDto());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute("userDto") UserDto userDto, RedirectAttributes redirectAttributes, UserService userService) {
+        userService.register(userDto);
+        redirectAttributes.addFlashAttribute("message", "User registered successfully!");
+        return "redirect:/login";
+    }
+}

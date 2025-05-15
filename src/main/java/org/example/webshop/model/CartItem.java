74fcbb7dbcb5b4c33994c689456c9213aaa18b11
ITHS <martinpaulsen617@gmail.com>
2025-05-15 19:58:49 +0200

    Uppdaterat product-list.html och skapat filen CartItem i package model

---
+++ src/main/java/org/example/webshop/model/CartItem.java
@@ -0,0 +1,18 @@
+package org.example.webshop.model;
+
+
+import org.example.webshop.entity.Product;
+
+public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() { return product; }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+}

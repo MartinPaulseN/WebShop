6b05032ac5f3e2b384550a723e8ea1125a46736c
ITHS <martinpaulsen617@gmail.com>
2025-05-16 14:44:15 +0200

    Skapat filen OrderItem i package entity

---
+++ src/main/java/org/example/webshop/entity/OrderItem.java
@@ -0,0 +1,58 @@
+package org.example.webshop.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private int quantity;
+
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

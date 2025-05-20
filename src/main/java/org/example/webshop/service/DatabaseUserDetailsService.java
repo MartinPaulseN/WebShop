84778d51e002bcc666043fe4358bd0a20bcb3fab
ITHS <martinpaulsen617@gmail.com>
2025-05-21 01:00:28 +0200

    Skapat filen DatabaseUserDetailsService i package service

---
+++ src/main/java/org/example/webshop/service/DatabaseUserDetailsService.java
@@ -0,0 +1,31 @@
+package org.example.webshop.service;
+
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.example.webshop.entity.User;
+import org.example.webshop.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public DatabaseUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Användare hittades inte: " + username));
+
+        UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(user.getUsername());
+        builder.password(user.getPassword());
+        builder.roles(user.getRole().replace("ROLE_", ""));
+
+        return builder.build();
+    }
+}
